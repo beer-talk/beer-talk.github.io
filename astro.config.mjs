e6e167c56c5c7a594e9176286155cbfd99d163d1
khoaipx <phamxuankhoai@gmail.com>
2026-03-15 04:18:16 +0700

    fix: correctly handle base path for github pages root domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE || 'https://beer-talk.github.io', // Dynamically passed by GitHub Actions
-  base: process.env.BASE || '/beer-talk', // Defaults to local, overrides on GH Pages
+  base: process.env.BASE || undefined, // Defaults to root, overridden by GH Pages subpaths if any
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
